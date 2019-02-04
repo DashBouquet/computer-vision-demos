afe97c1ccd04ee0331b94ad96dacccd55b09800d
Antony Shaleynikov <shaleynikov@gmail.com>
2019-02-04 17:30:13 +0300

    Updates

---
+++ precache-manifest.832ca3201f815b465c33e46ea24fe455.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b447901a61607ee00cd",
+    "url": "/computer-vision-demos/static/css/main.c4442a59.chunk.css"
+  },
+  {
+    "revision": "7b447901a61607ee00cd",
+    "url": "/computer-vision-demos/static/js/main.7b447901.chunk.js"
+  },
+  {
+    "revision": "28e1552cb90270e49b10",
+    "url": "/computer-vision-demos/static/js/1.28e1552c.chunk.js"
+  },
+  {
+    "revision": "d2d7e631e6f2bb54d83d",
+    "url": "/computer-vision-demos/static/js/runtime~main.d2d7e631.js"
+  },
+  {
+    "revision": "1eee18755074ec3501f9a04dc3708b13",
+    "url": "/computer-vision-demos/index.html"
+  }
+];
